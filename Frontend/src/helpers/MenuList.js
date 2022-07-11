0fc9f61bb57d8b1c3bf511919f2505ac1e9b7fab
saikirannain18 <108256654+saikirannain18@users.noreply.github.com>
2022-07-11 12:59:50 +0530

    Add files via upload

---
+++ Frontend/src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 250,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 330,
+  },
+  {
+    name: "PedroTech  Pizza",
+    image: PedroTechSpecial,
+    price: 390,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 225,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 340,
+  },
+  {
+    name: "VeryExpens Pizza",
+    image: Expensive,
+    price: 350,
+  },
+];
